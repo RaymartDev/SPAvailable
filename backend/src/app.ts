d196b5ebc95296cf651f8134116a477e3fc2cd47
Raymart <raymart.sarmiento23@outlook.com>
2024-03-12 02:00:57 +0800

    Updated app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,10 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
