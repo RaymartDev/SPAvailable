bdcf3f16954e93c45965e24e021f62c27e178868
Raymart <raymart.sarmiento23@outlook.com>
2024-03-12 00:07:35 +0800

    Updated

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app_1.default.listen(port, () => {
     /* eslint-disable no-console */
     console.log(`Listening: http://localhost:${port}`);
