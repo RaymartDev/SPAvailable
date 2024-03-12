ff45d50114ba66866fdbdca9631d606b8afda986
Raymart <raymart.sarmiento23@outlook.com>
2024-03-12 15:31:02 +0800

    Updated

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -38,6 +38,15 @@ const app = (0, express_1.default)();
 app.use((0, morgan_1.default)('dev'));
 app.use((0, helmet_1.default)());
 app.use((0, cors_1.default)());
+app.use(async (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+    next();
+});
 app.use(express_1.default.json({ limit: '10mb' }));
 app.use(express_1.default.urlencoded({ limit: '10mb', extended: true }));
 app.use((0, cookie_parser_1.default)());
