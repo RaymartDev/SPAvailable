0be750f9f6eec1536619d0cbc93f17c468c1e18e
Raymart <raymart.sarmiento23@outlook.com>
2024-03-12 15:26:05 +0800

    Updated

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -37,11 +37,7 @@ require('dotenv').config();
 const app = (0, express_1.default)();
 app.use((0, morgan_1.default)('dev'));
 app.use((0, helmet_1.default)());
-app.use((0, cors_1.default)({
-    origin: '*',
-    allowedHeaders: '*',
-    methods: ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE'],
-}));
+app.use((0, cors_1.default)());
 app.use(express_1.default.json({ limit: '10mb' }));
 app.use(express_1.default.urlencoded({ limit: '10mb', extended: true }));
 app.use((0, cookie_parser_1.default)());
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,11 +14,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors({
-  origin: '*',
-  allowedHeaders: '*',
-  methods: ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE'],
-}));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
